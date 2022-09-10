1613cf2e0dd76be9f6232142ec5b4db1aa0fd5b4
scornea <scornea3@gmail.com>
2022-09-10 09:13:44 +0100

    strrev

--- strrev.go
+++ strrev.go
@@ -2,7 +2,7 @@ package piscine
 
 func StrRev(s string) string {
 	runes := []rune(s)
-	for i, j := 0, len(runes)-1; 1 < j; i, j = i+1, j-1 {
+	for i, j := 0, len(runes)-1; 1 < j; i, j = i+i, j-i {
 		runes[i], runes[j] = runes[j], runes[i]
 	}
 	return string(runes)

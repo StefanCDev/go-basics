0c747cd653f2bc4745f9793cc2cb8628318cf201
scornea <scornea3@gmail.com>
2022-09-13 12:09:22 +0100

    recursive

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,8 +1,15 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
+	if nb > 21 {
+		return 0
+	}
+	if nb < 1 {
+		return 0
+	}
 	if nb <= 1 {
 		return 1
+	} else {
+		return nb * RecursiveFactorial(nb-1)
 	}
-	return nb * RecursiveFactorial(nb-1)
 }

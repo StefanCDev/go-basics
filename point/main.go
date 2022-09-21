ceadd7ae5ebc949309f79972677282249df8a0bd
scornea <scornea3@gmail.com>
2022-09-21 11:54:18 +0100

    point

---
+++ point/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("File name missing")
+	} else if len(arguments) > 2 {
+		fmt.Println("Too many arguments")
+	} else {
+		fileName := string(arguments[1])
+		file, err := os.Open(fileName)
+		if err != nil {
+			fmt.Printf("The mistake is: %v\n", err.Error())
+		}
+		arr := make([]byte, 14)
+		file.Read(arr)
+		fmt.Println(string(arr))
+		file.Close()
+	}
+}

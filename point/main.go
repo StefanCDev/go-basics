3c28ba5197ea97c187500f1cb89c767daa358253
scornea <scornea3@gmail.com>
2022-09-21 11:56:36 +0100

    point

--- point/main.go
+++ point/main.go
@@ -1,25 +1,46 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"github.com/01-edu/z01"
 )
 
-func main() {
-	arguments := os.Args
-	if len(arguments) == 1 {
-		fmt.Println("File name missing")
-	} else if len(arguments) > 2 {
-		fmt.Println("Too many arguments")
-	} else {
-		fileName := string(arguments[1])
-		file, err := os.Open(fileName)
-		if err != nil {
-			fmt.Printf("The mistake is: %v\n", err.Error())
-		}
-		arr := make([]byte, 14)
-		file.Read(arr)
-		fmt.Println(string(arr))
-		file.Close()
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
 	}
+}
+
+func StripDigits(n int) []int {
+	if n < 10 {
+		return []int{n}
+	} else {
+		return append(StripDigits(n/10), []int{n % 10}...)
+	}
+}
+
+func setPoint(ptr *[2]int) {
+	ptrArr := *ptr
+	ptrArr[0] = 42
+	ptrArr[1] = 21
+	*ptr = ptrArr
+}
+
+func main() {
+	points := [2]int{}
+	numArr := make([]int, 0)
+
+	setPoint(&points)
+
+	numArr = StripDigits(points[0])
+
+	printStr("x = ")
+	for _, r := range numArr {
+		z01.PrintRune(rune(r + 48))
+	}
+	printStr(", y = ")
+	numArr = StripDigits(points[1])
+	for _, r := range numArr {
+		z01.PrintRune(rune(r + 48))
+	}
+	z01.PrintRune('\n')
 }

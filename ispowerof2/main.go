697a51cdaaaf2c71109c304b7e25f1bd49d37098
scornea <scornea3@gmail.com>
2022-09-22 15:39:20 +0100

    ispowerof2

---
+++ ispowerof2/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os"
+	"strconv"
+)
+
+func main() {
+	for len(os.Args) == 2 {
+		numbers, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			panic(err)
+		}
+		newnum := numbers
+		counts := 0
+		for numbers != 1 {
+			if newnum%2 != 0 {
+			} else {
+				newnum = newnum / 2
+			}
+			counts++
+		}
+		var x int = 2 ^ counts
+		if x == numbers {
+			return true
+		} else {
+			return false
+		}
+	}
+}
